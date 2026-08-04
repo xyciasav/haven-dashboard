83a8b764cdcc7fdc12af531e308043242da89f55
Mike Rodriguez <michaelrodriguezphotography@gmail.com>
2026-08-04 14:03:36 -0700

    Keep async request failures behind JSON boundary

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'haven-shell-v0.6.1';
-const SHELL = ['/', '/index.html', '/styles.css?v=0.6.1', '/settings.css?v=0.6.1', '/security.css?v=0.6.1', '/version.css?v=0.6.1', '/keycloak.css?v=0.6.1', '/recovery.css?v=0.6.1', '/app-manager.css?v=0.6.1', '/integrations.css?v=0.6.1', '/app.js?v=0.6.1', '/vendor/keycloak.js', '/manifest.webmanifest', '/icons/haven.svg', '/icons/haven-192.png', '/icons/haven-512.png'];
+const CACHE = 'haven-shell-v0.6.2';
+const SHELL = ['/', '/index.html', '/styles.css?v=0.6.2', '/settings.css?v=0.6.2', '/security.css?v=0.6.2', '/version.css?v=0.6.2', '/keycloak.css?v=0.6.2', '/recovery.css?v=0.6.2', '/app-manager.css?v=0.6.2', '/integrations.css?v=0.6.2', '/app.js?v=0.6.2', '/vendor/keycloak.js', '/manifest.webmanifest', '/icons/haven.svg', '/icons/haven-192.png', '/icons/haven-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
